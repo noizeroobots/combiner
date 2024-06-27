0c2ebbc40baef0ccdb1230db0f5a57f71edef6d4
aleksey <noizeroobots@gmail.com>
2024-06-27 22:05:17 +0300

    first lessons

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white">
+    <a href="#">
+      <img src="./assets/logo.png" alt="Logo" class="h-9">
+    </a>
+
+    <a href="#" class="text-sm">
+      <div v-if="true" class="flex items-center gap-1">
+        Day complete!
+        <CheckCircleIcon class="h-7 text-green-500"/>
+      </div>
+
+      <div v-else class="flex items-center gap-1">
+    <div>Progress: <span class="font-mono">20%</span></div>
+
+    <div class="h-3 w-3 rounded-full bg-red-500"></div>
+      </div>
+
+    </a>
+
+  </header>
+
+
+</template>
